2841860ee23c45e5c7ad39c3555e7246f3123a7c
Akhil Chandran <akhil9216@gmail.com>
2022-06-11 22:51:49 +0100

    scrolling issue solved

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -52,23 +52,25 @@ class Tooltip extends Component{
     create(){
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
-        tooltipElement.textContent = this.text;
-        tooltipElement.innerHTML = `
-        <h2>More Info</h2>
-        <p>${this.text}</p>`
+        const tooltipTemplate = document.getElementById('tooltip');
+        const tooltipBody = document.importNode(tooltipTemplate.content, true);
+        tooltipBody.querySelector('p').textContent = this.text;
+        tooltipElement.append(tooltipBody);
+        
 
         const hostElPosLeft = this.hostElement.offsetLeft;
         const hostElPosTop = this.hostElement.offsetTop;
         const hostElPosHeight = this.hostElement.clientHeight;
+        const parentElementScrolling = this.hostElement.parentElement.scrollTop;
 
         const x = hostElPosLeft + 20;
-        const y = hostElPosTop + hostElPosHeight -10;
+        const y = hostElPosTop + hostElPosHeight - parentElementScrolling - 10;
 
         tooltipElement.style.position = 'absolute';
         tooltipElement.style.left = x + 'px';
         tooltipElement.style.top = y + 'px';
 
-        tooltipElement.addEventListener('click', this.detach);
+        tooltipElement.addEventListener('click', this.closeTooltip);
         this.element = tooltipElement;
     }
 }
